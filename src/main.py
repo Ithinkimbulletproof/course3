8dce7f792b6b2d53ba6f4fb8e406bc82cc8aa5c6
Alexandr Muhametchin <devilmaker228@gmail.com>
2023-12-24 21:04:29 +0700

    Get all executed operations

--- src/main.py
+++ src/main.py
@@ -1,32 +1,33 @@
 import json
 from datetime import datetime, date, time
 
-def __init__(self):
-    self.list_transfer = []
+class Operations:
+    def __init__(self):
+        self.list_transfer = []
 
-def __repr__(self):
-    return(f"Last user's operations\n"
-               f"{self.list_transfer}")
+    def __repr__(self):
+        return (f"Last user's operations\n"
+                f"{self.list_transfer}")
 
-def load_operations(self):
-    """Функция распаковывает json-файл"""
-    with open("operations.json", encoding="utf-8") as file:
-        self.list_transfer = json.loads(file.read())
-    return self.list_transfer
+    def load_operations(self):
+        """Функция распаковывает json-файл"""
+        with open(r"C:\PycharmProjects\course3\operations.json", encoding="utf-8") as file:
+            self.list_transfer = json.loads(file.read())
+        return self.list_transfer
 
-def get_last_five_operations(self):
-    """Функция показывает последние 5 операций"""
-    five_list = []
-    for state in self.list_transfer:
-        ex_state = state.get('state')
-        if ex_state == "EXECUTED":
-            five_list.append(state)
-        else:
-            continue
+    def get_executed_operations(self, transfers_list):
+        """Функция показывает операции,
+        которые имеют статус EXECUTED"""
+        ex_operation_list = []
+        for state in transfers_list:
+            if state.get("state") == "EXECUTED":
+                ex_operation_list.append(state)
+        self.list_transfer = ex_operation_list
+        return ex_operation_list
 
-    return five_list
+def main():
+    operation = Operations()
+    print(operation.load_operations())
+    print(operation.get_executed_operations(operation.list_transfer))
 
-print(get_last_five_operations())
-#    def
-
-
+main()
