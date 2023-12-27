b636c71c0fa2d096fd499fb8e6fbecec18b0941c
Alexandr Muhametchin <devilmaker228@gmail.com>
2023-12-27 12:38:55 +0700

    Get date

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime, date, time
 
 class Operations:
     def __init__(self):
@@ -11,23 +10,34 @@ class Operations:
 
     def load_operations(self):
         """Функция распаковывает json-файл"""
-        with open(r"C:\PycharmProjects\course3\operations.json", encoding="utf-8") as file:
+        with open(r"C:\PycharmProjects\course3\operations.json",
+                  encoding="utf-8") as file:
             self.list_transfer = json.loads(file.read())
         return self.list_transfer
 
-    def get_executed_operations(self, transfers_list):
-        """Функция показывает операции,
-        которые имеют статус EXECUTED"""
-        ex_operation_list = []
-        for state in transfers_list:
-            if state.get("state") == "EXECUTED":
-                ex_operation_list.append(state)
-        self.list_transfer = ex_operation_list
-        return ex_operation_list
+#    def get_executed_operations(self, transfers_list):
+#        """Функция показывает операции,
+#        которые имеют статус EXECUTED"""
+#        ex_operation_list = []
+#        for state in transfers_list:
+#            if state.get("state") == "EXECUTED":
+#                ex_operation_list.append(state)
+#        self.list_transfer = ex_operation_list
+#        return ex_operation_list
+
+    @staticmethod
+    def get_date(transfers_list):
+        ex_peration_list = [item for item in transfers_list if
+                            item.get('state') == "EXECUTED"]
+        ex_peration_list.sort(key=lambda x: x.get('date'),
+                              reverse=True)
+        return ex_peration_list
+
 
 def main():
     operation = Operations()
     print(operation.load_operations())
-    print(operation.get_executed_operations(operation.list_transfer))
+    print(operation.get_date(operation.list_transfer))
+
 
 main()
